58b52890227b3f4455510dfc952a24e90f7ef3a6
DrPleasure <114397562+DrPleasure@users.noreply.github.com>
2022-11-29 16:51:12 +0100

    .

--- src/components/SingleBook.jsx
+++ src/components/SingleBook.jsx
@@ -1,5 +1,5 @@
-import { Component } from 'react'
-import { Card } from 'react-bootstrap'
+import { Component } from "react";
+import { Card } from "react-bootstrap";
 // import CommentArea from './CommentArea'
 
 class SingleBook extends Component {
@@ -16,13 +16,13 @@ class SingleBook extends Component {
           style={{
             border:
               this.props.selectedBook === this.props.book.asin
-                ? '3px solid red'
-                : 'none',
+                ? "3px solid red"
+                : "none",
           }}
         >
           <Card.Img variant="top" src={this.props.book.img} />
           <Card.Body>
-            <Card.Title style={{ color: 'black' }}>
+            <Card.Title style={{ color: "black" }}>
               {this.props.book.title}
             </Card.Title>
           </Card.Body>
@@ -31,8 +31,8 @@ class SingleBook extends Component {
           this.state.selected && <CommentArea asin={this.props.book.asin} />
         } */}
       </>
-    )
+    );
   }
 }
 
-export default SingleBook
+export default SingleBook;
